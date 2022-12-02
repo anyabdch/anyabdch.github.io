efa69351cc454e85dbafad30c575a3483409ce8a
anyabdch <116769112+anyabdch@users.noreply.github.com>
2022-12-01 18:56:09 -0600

    Add files via upload

---
+++ Final Project/main.js
@@ -0,0 +1,45 @@
+var projects = [
+    {
+        title: "PocketConcert",
+        cover: "SpotifyLogo.png",
+        desc: "Spotify based recommendation software"
+    },
+    {
+        title: "TicTacToe",
+        cover: "TicTacToe.png",
+        desc: "Play tic-tac-toe against AI"
+    }
+]
+
+function listProjects(projects){
+    let portfolio = document.querySelector("#projects");
+    if (portfolio){
+      if(projects instanceof Array){
+        for (let project of projects){
+            let html = `
+                    <img class="p-img" src="images/${project.cover}">
+                        <a class="deets" rel="${project.title} Repository" href="https://anyabdch.github.io/${project.title}">
+                            <h1>${project.title}</h1>
+                            <p>${project.desc}</p>
+                        </a>
+                </div>
+                `
+            let container = document.createElement("div");
+            container.className = "proj";
+            container.id = project.title
+            container.innerHTML = html;
+            container.tabIndex = projects.indexOf(project);
+            portfolio.append(container);
+        }
+  
+      }
+  
+    }
+  
+  }
+
+function openClose(target){
+    let obj = document.getElementById(target);
+    obj.classList.toggle("hidden");
+}
+
