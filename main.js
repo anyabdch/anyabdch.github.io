c3e1ad4e40402feb0a7c1f6994123e8f58581c56
Anya <116769112+anyabdch@users.noreply.github.com>
2023-01-07 22:54:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ var projects = [
     {
         title: "PocketConcert",
         cover: "SpotifyLogo.png",
-        desc: "Spotify based recommendation software"
+        desc: "Spotify based recommendation software",
+        idx: False
     },
     {
         title: "TicTacToe",
         cover: "TicTacToe.png",
-        desc: "Play tic-tac-toe against AI"
+        desc: "Play tic-tac-toe against AI",
+        idx: False
     },
     {
         title: "DirectFlix",
         cover: "net_logo.png",
-        desc: "Netflix random genre generator"
+        desc: "Netflix random genre generator",
+        idx: True
     }
 ]
 
@@ -21,14 +24,26 @@ function listProjects(projects){
     if (portfolio){
       if(projects instanceof Array){
         for (let project of projects){
-            let html = `
-                    <img class="p-img" src="images/${project.cover}">
-                        <a class="deets" rel="${project.title} Repository" href="https://github.com/anyabdch/${project.title}">
-                            <h1>${project.title}</h1>
-                            <p>${project.desc}</p>
-                        </a>
-                </div>
-                `
+            if(project.idx){
+                let html = `
+                        <img class="p-img" src="images/${project.cover}">
+                            <a class="deets" rel="${project.title} Repository" href="https://github.com/anyabdch/${project.title}/index.html">
+                                <h1>${project.title}</h1>
+                                <p>${project.desc}</p>
+                            </a>
+                    </div>
+                    `
+                }
+            else{
+                let html = `
+                        <img class="p-img" src="images/${project.cover}">
+                            <a class="deets" rel="${project.title} Repository" href="https://github.com/anyabdch/${project.title}">
+                                <h1>${project.title}</h1>
+                                <p>${project.desc}</p>
+                            </a>
+                    </div>
+                    `
+                }
             let container = document.createElement("div");
             container.className = "proj";
             container.id = project.title
